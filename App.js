464be7e333f95ccf39ac79f4e32aceef3aed2822
Elias Colturato da Silva <91541899+EliasColturato@users.noreply.github.com>
2022-12-10 12:32:52 -0300

    Criação do Projeto
    O projeto foi criado utilizando o expo go e o Axios para realizar
    requisições para a api que será utilizada

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/pokedexLogo.png')} style={styles.logo} />
+      <Text>This is my first app on React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +17,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  logo: {
+    width: '80%',
+    resizeMode: 'contain',
   },
 });
